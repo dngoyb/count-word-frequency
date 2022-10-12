0408e7265cb4fd216a0a9dd5ac8f3b79b0aa27f1
Ngoy <dngoyb@gmail.com>
2022-10-12 21:35:35 +0200

    every word in the sentence is counted

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const sentence = 'the the, not bad 55 ?Greek, The Not so good';
+const sentence =
+	'Plat and play the the, not bad 55 ?Greek, The Not so good and play';
+
+const frequencies = {};
+const str = sentence
+	.toLowerCase()
+	.replace(/[^\w -]/g, '')
+	.split(' ');
+
+for (const word of str) {
+	if (word) {
+		frequencies[word] ? frequencies[word]++ : (frequencies[word] = 1);
+	}
+}
+
+console.log(frequencies);
