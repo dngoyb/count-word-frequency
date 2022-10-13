262883ebd7dc9deb9014f55beee7dedcb88eb6de
Ngoy <dngoyb@gmail.com>
2022-10-13 06:20:00 +0200

    collected words from Input and displayed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const sentence =
-	'Plat and play the the, not bad 55 ?Greek, The Not so good and play';
+const sentence = document.getElementById('input');
+const button = document.querySelector('button');
+const ol = document.querySelector('ul');
 
 const frequencies = {};
 const frequenciesArray = [];
 
-const str = sentence
+const str = sentence.value
 	.toLowerCase()
 	.replace(/[^\w -]/g, '')
 	.split(' ');
@@ -24,10 +25,14 @@ for (let key in frequencies) {
 
 frequenciesArray.sort((a, b) => b.count - a.count);
 
-for (let i = 0; i < 3; i++) {
-	console.log(
-		`${i}. ${frequenciesArray[i].word.toUpperCase()}: was counted ${
+button.addEventListener('click', (e) => {
+	let position = 0;
+	ol.innerText = '';
+	for (let i = 0; i < 3; i++) {
+		const li = document.createElement('li');
+		li.innerHTML = ` ${frequenciesArray[i].word.toUpperCase()}: was counted ${
 			frequenciesArray[i].count
-		}`
-	);
-}
+		}`;
+		ol.appendChild(li);
+	}
+});
